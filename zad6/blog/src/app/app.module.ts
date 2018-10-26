485215b416136da7e05ac4a3a8fe714f0d9114d2
DaQa925@gmail.com <31065@student.pwsz.tar.edu.pl>
2018-10-26 12:12:35 +0200

    lab5

--- zad6/blog/src/app/app.module.ts
+++ zad6/blog/src/app/app.module.ts
@@ -1,18 +1,53 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { QuizComponent } from './components/quiz/quiz.component';
+import { HomeComponentComponent } from './components/home-component/home-component.component';
+import { AppNavbarComponent } from './components/app-navbar/app-navbar.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { BlogItemComponent } from './components/blog/blog-item/blog-item.component';
+import { BlogItemTextComponent } from './components/blog/blog-item-text/blog-item-text.component';
+import { SummaryPipe } from './summary.pipe';
+import {BlogItemImageComponent} from './components/blog/blog-item-image/blog-item-image.component';
+
+const appRoutes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponentComponent
+  },
+  {
+    path: 'contact',
+    component: ContactComponent,
+  },
+  {
+    path: 'quiz',
+   component: QuizComponent,
+  },
+  {
+    path: 'blog',
+    component: BlogComponent,
+  }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     ContactComponent,
-    QuizComponent
+    QuizComponent,
+    HomeComponentComponent,
+    AppNavbarComponent,
+    BlogComponent,
+    BlogItemComponent,
+    BlogItemTextComponent,
+    SummaryPipe,
+    BlogItemImageComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
